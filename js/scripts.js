a477fd5cdb2336a910068b48f06fe3197d184c04
Lucas Lemonnier <ll@ttwebco.com>
2015-04-14 18:24:46 -0400

    created function for displayTime. Added setInterval() to update every second.

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,57 @@
-// Today
-now = new Date();
+displayTime = function(){
 
-// Get current hours, minutes, seconds
-var hour = now.getHours();
-var mins = now.getMinutes();
-var secs = now.getSeconds();
+	// Today
+	now = new Date();
 
-// Define am and pm
-var ap = 'am';
-if (hour > 11) ap = 'pm';
+	// Get current hours, minutes, seconds
+	var hour = now.getHours();
+	var mins = now.getMinutes();
+	var secs = now.getSeconds();
 
-// Define hours in 12-hour format
-if (hour > 12) hour = hour - 12;
-if (hour == 0) hour = 12;
+	// Define hours in 12-hour format
+	if (hour > 12) hour = hour - 12;
+	if (hour == 0) hour = 12;
 
-// Set hours, minutes, and seconds as double digits
-if (hour < 10) hour = '0' + hour;
-if (mins < 10) mins = '0' + mins;
-if (secs < 10) secs = '0' + secs;
+	// Set hours, minutes, and seconds as double digits
+	if (hour < 10) hour = '0' + hour;
+	if (mins < 10) mins = '0' + mins;
+	if (secs < 10) secs = '0' + secs;
 
-// Add hours to .number-box-hours .number
-var hoursElement = document.getElementById('hours');
-hoursElement.textContent = hour;
+	// Add hours to .number-box-hours .number
+	var hoursElement = document.getElementById('hours');
+	hoursElement.textContent = hour;
 
-// Add minutes to .number-box-minutes .number
-var minsElement = document.getElementById('minutes');
-minsElement.textContent = mins;
+	// Add minutes to .number-box-minutes .number
+	var minsElement = document.getElementById('minutes');
+	minsElement.textContent = mins;
 
-// Add seconds to .number-box-seconds .number
-var secsElement = document.getElementById('seconds');
-secsElement.textContent = secs;
+	// Add seconds to .number-box-seconds .number
+	var secsElement = document.getElementById('seconds');
+	secsElement.textContent = secs;
+
+	// Define am and pm
+	var meridiem = 'am';
+	if (hour > 12) {
+    hour = hour - 12;
+    meridiem = "pm";
+	}
+
+	// Add am meridiem to .ampm .am
+	var am = document.getElementById('am');
+	am.textContent = meridiem;
+
+	// Add pm meridiem to .ampm .pm
+	var pm = document.getElementById('pm');
+	pm.textContent = meridiem;
+}
+
 
 // If am, hide pm
 // if pm, hide am
 
 $(document).ready(function() {
 
-	console.log(hour);
-	console.log(mins);
-	console.log(secs);
-	console.log(ap);
+	displayTime();
+	setInterval(displayTime, 1000);
 
 });
