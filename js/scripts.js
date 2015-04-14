e78819d23835ec659f14673678e6e312e26ced8e
Lucas Lemonnier <ll@ttwebco.com>
2015-04-14 17:54:02 -0400

    hours, mins, secs dynamically added to page

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,10 @@
+// Today
 now = new Date();
 
-// Get hours
+// Get current hours, minutes, seconds
 var hour = now.getHours();
-	// append them to .number-box-hours .number
-
-// Get minutes
 var mins = now.getMinutes();
-	// append them to .number-box-minutes .number
-
-// Get seconds
 var secs = now.getSeconds();
-	// append them to .number-box-seconds .number
 
 // Define am and pm
 var ap = 'am';
@@ -25,14 +19,26 @@ if (hour < 10) hour = '0' + hour;
 if (mins < 10) mins = '0' + mins;
 if (secs < 10) secs = '0' + secs;
 
-	// If am, hide pm
-	// if pm, hide am
+// Add hours to .number-box-hours .number
+var hoursElement = document.getElementById('hours');
+hoursElement.textContent = hour;
+
+// Add minutes to .number-box-minutes .number
+var minsElement = document.getElementById('minutes');
+minsElement.textContent = mins;
+
+// Add seconds to .number-box-seconds .number
+var secsElement = document.getElementById('seconds');
+secsElement.textContent = secs;
+
+// If am, hide pm
+// if pm, hide am
 
 $(document).ready(function() {
 
 	console.log(hour);
 	console.log(mins);
 	console.log(secs);
-
+	console.log(ap);
 
 });
