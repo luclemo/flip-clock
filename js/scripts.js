b1c9dd72efae60ac0adcb0c9a5c6deba01f8981c
Lucas Lemonnier <ll@ttwebco.com>
2015-04-15 08:06:28 -0400

    integtrated meridiem

--- js/scripts.js
+++ js/scripts.js
@@ -8,46 +8,47 @@ displayTime = function(){
 	var mins = now.getMinutes();
 	var secs = now.getSeconds();
 
+	// Set variable for meridiem
+	var meridiem = 'am';
+
 	// Define hours in 12-hour format
-	if (hour > 12) hour = hour - 12;
-	if (hour == 0) hour = 12;
+	// Redefine meridiem based on 12-hour format
+	if (hour > 12) {
+		hour = hour - 12;
+		meridiem = 'pm';
+	}
+	if (hour == 0) {
+		hour = 12;
+	}
 
 	// Set hours, minutes, and seconds as double digits
 	if (hour < 10) hour = '0' + hour;
 	if (mins < 10) mins = '0' + mins;
 	if (secs < 10) secs = '0' + secs;
 
-	// Add hours to .number-box-hours .number
+	// Add hours to clock
 	var hoursElement = document.getElementById('hours');
 	hoursElement.textContent = hour;
 
-	// Add minutes to .number-box-minutes .number
+	// Add minutes to clock
 	var minsElement = document.getElementById('minutes');
 	minsElement.textContent = mins;
 
-	// Add seconds to .number-box-seconds .number
+	// Add seconds to clock
 	var secsElement = document.getElementById('seconds');
 	secsElement.textContent = secs;
 
-	// Define am and pm
-	var meridiem = 'am';
-	if (hour > 12) {
-    hour = hour - 12;
-    meridiem = "pm";
+	// Add meridiem to clock
+	var am = document.getElementById('am');
+	var pm = document.getElementById('pm');
+	
+	if (meridiem === 'am') {
+		am.textContent = meridiem;	
+	} else{
+		pm.textContent = meridiem;
 	}
 
-	// Add am meridiem to .ampm .am
-	var am = document.getElementById('am');
-	am.textContent = meridiem;
-
-	// Add pm meridiem to .ampm .pm
-	var pm = document.getElementById('pm');
-	pm.textContent = meridiem;
 }
-
-
-// If am, hide pm
-// if pm, hide am
 
 $(document).ready(function() {
 
